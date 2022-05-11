44c53494520f7e4423238b9780a4ae1a3dcadf86
ThunderGod77 <kshitijgang76@gmail.com>
2022-05-11 10:42:33 +0530

    initial setyp

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
+  },
+  plugins: [],
+};
